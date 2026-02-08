2d61000888df47ca8d5190a90d9ad4d850ab9ff2
Xizhe Zhang <zhangxizhe2016@gmail.com>
2026-02-08 09:50:37 +0800

    123

--- evaluation/run_eval.sh
+++ evaluation/run_eval.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-python -m evaluation.r3meval.core.hydra_launcher --multirun \
-  hydra/launcher=basic hydra/sweeper=basic hydra/output=local \
-  env="kitchen_micro_open-v3" \
-  camera="left_cap" \
-  pixel_based=true \
-  embedding=resnet50 \
-  num_demos=5 \
-  env_kwargs.load_path=r3m \
-  bc_kwargs.finetune=false \
-  proprio=9 \
-  job_name=try_r3m
+#python -m evaluation.r3meval.core.hydra_launcher --multirun \
+#  hydra/launcher=basic hydra/sweeper=basic hydra/output=local \
+#  env="kitchen_micro_open-v3" \
+#  camera="left_cap" \
+#  pixel_based=true \
+#  embedding=resnet50 \
+#  num_demos=5 \
+#  env_kwargs.load_path=r3m \
+#  bc_kwargs.finetune=false \
+#  proprio=9 \
+#  job_name=try_r3m
 
 python hydra_launcher.py --multirun \
                           hydra/launcher=local \
