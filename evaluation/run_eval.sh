709d6d3b281a183cc18798d37e037829902433a7
Xizhe Zhang <zhangxizhe2016@gmail.com>
2026-02-14 17:14:18 +0800

    Update run_eval.sh

--- evaluation/run_eval.sh
+++ evaluation/run_eval.sh
@@ -6,9 +6,9 @@ export GPUS=""
 python -m r3meval.core.hydra_launcher --multirun \
   hydra/launcher=local hydra/output=local \
   hydra.sweep.subdir='job_${hydra.job.num}' \
-  device=cpu \
+  device=cpu\
   eval_frequency=1000 \
-  env=kitchen_knob1_on-v3,kitchen_light_on-v3,kitchen_sdoor_open-v3,kitchen_ldoor_open-v3,kitchen_micro_open-v3\
+  env=kitchen_knob1_on-v3\
   camera=left_cap2 \
   pixel_based=true \
   embedding=resnet50 \
@@ -16,9 +16,7 @@ python -m r3meval.core.hydra_launcher --multirun \
   env_kwargs.load_path=r3m \
   bc_kwargs.finetune=false \
   proprio=9 \
-  job_name=r3m_baseline_num_1
-
-
+  job_name=r3m_smoke_test
 
 
 #env=kitchen_knob1_on-v3,kitchen_light_on-v3,kitchen_sdoor_open-v3,kitchen_ldoor_open-v3,kitchen_micro_open-v3 \
