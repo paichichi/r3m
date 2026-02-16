a735ea273716fffc75a90f3b607434f106cb78be
Xizhe Zhang <zhangxizhe2016@gmail.com>
2026-02-16 10:22:07 +0800

    112233

--- evaluation/run_eval.sh
+++ evaluation/run_eval.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=""
-export GPUS=""
+#export CUDA_VISIBLE_DEVICES=""
+#export GPUS=""
 
 python -m r3meval.core.hydra_launcher --multirun \
   hydra/launcher=local hydra/output=local \
   hydra.sweep.subdir='job_${hydra.job.num}' \
-  device=cpu\
+  device=cpu \
   eval_frequency=1000 \
-  env=kitchen_knob1_on-v3\
+  env=kitchen_knob1_on-v3,kitchen_light_on-v3,kitchen_sdoor_open-v3,kitchen_ldoor_open-v3,kitchen_micro_open-v3 \
   camera=left_cap2 \
   pixel_based=true \
   embedding=resnet50 \
@@ -16,7 +16,7 @@ python -m r3meval.core.hydra_launcher --multirun \
   env_kwargs.load_path=r3m \
   bc_kwargs.finetune=false \
   proprio=9 \
-  job_name=r3m_smoke_test
+  job_name=r3m_eval_num_1_285k
 
 
 #env=kitchen_knob1_on-v3,kitchen_light_on-v3,kitchen_sdoor_open-v3,kitchen_ldoor_open-v3,kitchen_micro_open-v3 \
